4577f010dc62f8dbefb4313235328e2e9025d4d7
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-25 21:02:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+from pyrogram import Client, idle
+from Config import Config
+
+# Set the global logging level to WARNING to show only warnings and errors
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+# Adjust specific logging levels for different libraries
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("PIL").setLevel(logging.WARNING)
+
+app = Client(
+    "UnzipBot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="plugins"),
+)
+
+# Run Bot
+if __name__ == "__main__":
+    app.start()  # Not using run as wanna print...
+    uname = app.get_me().username
+    print(f"@{uname} Started Successfully!")
+    idle()
+    app.stop()
+    print("Bot stopped. Alvida!")
