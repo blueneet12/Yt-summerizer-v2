1fa22fc502dcbb139553f3f242d9acafde994613
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-25 21:32:17 +0530

    Update general.py

--- plugins/general.py
+++ plugins/general.py
@@ -1,6 +1,15 @@
-from database import db
+import os
+import re
+import asyncio
+import speech_recognition as sr
+from pydub import AudioSegment
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import YouTube
+from groq import Groq
 from pyrogram import Client, filters
+from youtube_transcript_api.formatters import JSONFormatter
 from config import Config, Ai, Telegram
+from database import db
 
 
 @Client.on_message(filters.command('start'))
