b6b93e42da590fe3cd73c2524598b5906538ee66
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-26 00:07:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import os
 
-class Config:
+class Telegram:
     API_ID = os.environ.get('API_ID', 10471716)
     API_HASH = os.environ.get('API_HASH', 'f8a1b21a13af154596e2ff5bed164860')
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '6916875347:AAEVxR4cO_sIBB6V57ANA92pHKxzw9G3yX0')
-
-
-class Telegram:
     AUTH_USER_ID = os.environ.get('AUTH_USER_ID', '6883997969')
-
-
+    
 class Ai:
     GROQ_API_KEY = os.environ.get('GROQ_API_KEY', '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')
 
